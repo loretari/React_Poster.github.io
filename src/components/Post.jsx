7a8928c32b6b3e1faae8557fcd1f0238328c7340
loretari <loretari@gmail.com>
2023-07-11 15:47:14 +0300

    init: State & Conditional Content

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,7 @@ function Post(props) {
 
     return (
         <li className= {classes.post}>
-            <p className={classes.author}>{props.auth}</p>
+            <p className={classes.author}>{props.author}</p>
             <p className={classes.text}>{props.body}</p>
         </li>
 
