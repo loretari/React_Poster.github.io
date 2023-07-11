f0927a1315f856e13da0c87d0c43333ddccab9b5
loretari <loretari@gmail.com>
2023-07-11 10:57:46 +0300

    init: Reusing Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import Post from './components/Post'
 
-function App() {
-    return <Post />
-}
+    function App() {
+        return (
+            <main>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </main>
+        )
 
-export default App;
+
+    }
+
+    export default App;
