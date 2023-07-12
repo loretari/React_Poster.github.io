3a209af1e9d2e0f9de1996ef0053f5ce6a441614
loretari <loretari@gmail.com>
2023-07-12 10:06:46 +0300

    init: Outputting List Data

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -21,7 +21,7 @@ function NewPost({ onCancel, onAddPost} ) {
         event.preventDefault();
         const postData = {
             body: enteredBody,
-            authorChangeHandler: enteredAuthor
+            author: enteredAuthor
         }
        onAddPost(postData);
         onCancel();
