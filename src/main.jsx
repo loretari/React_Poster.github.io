45d9f1f97a223b4d8a5e3202d13d13e1863ec344
loretari <loretari@gmail.com>
2023-07-12 14:50:34 +0300

    init: Refactoring Route Components & More Nesting

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import App from './App'
+import Posts from './routes/Posts'
 import './index.css'
-import NewPost from "./components/NewPost";
+import NewPost from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
 
 
@@ -11,8 +11,12 @@ const router = createBrowserRouter([
     {path: '/',
     element: <RootLayout/>,
     children: [
-        { path: '/', element: <App/>},
-        {path: '/create-post', element: <NewPost />},
+        { path: '/', element: <Posts/>,
+        children: [
+            {path: '/create-post', element: <NewPost />},
+        ]
+        },
+
     ],
     },
 
